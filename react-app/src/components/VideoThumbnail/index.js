1e9f5951c130ff53bcde6f5b4c586bb8db10cfab
thisismydisplay <markrockwellosman@gmail.com>
2022-06-10 17:57:13 -0500

    feat:video page working

--- react-app/src/components/VideoThumbnail/index.js
+++ react-app/src/components/VideoThumbnail/index.js
@@ -1,38 +1,40 @@
-// import './VideoThumbnail.css';
-// import '../../index.css';
-// import ReactPlayer from 'react-player';
-// import { findDOMNode } from 'react-dom';
+import './VideoThumbnail.css';
+import '../../index.css';
+import ReactPlayer from 'react-player';
+import { findDOMNode } from 'react-dom';
 
-// import React, { useState } from 'react';
-// import { useSelector } from 'react-redux';
-// import { selectProfile } from '../../store/profile';
-// import { Redirect } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectProfile } from '../../store/profile';
+import { Redirect, useHistory } from 'react-router-dom';
 
-// function VideoThumbnail({ video, profile }) {
-//     const profile = useSelector(
-//         (state) => state.profile.profiles[state.profile.currentProfileId]
-//     );
-//     const [isHover, setIsHover] = useState(false)
-//     //HOVER SETS ALL VIDEOS, NEED TO SEPERATE INTO COMPONENT
+function VideoThumbnail({ video, profile }) {
+    const [isHover, setIsHover] = useState(false);
+    const history = useHistory();
+    const handleClick = () => {
+        history.push(`/video/${video.id}`)
+    }
 
-//     console.log(profile);
-//     if (!profile) return <Redirect to='/profile' />;
-//     return (
+    return (
+        <div
+            className='video-thumb-wrapper'
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}
+            key={video.id}
+            onClick={handleClick}
+        >
+            <ReactPlayer
+                volume={0}
+                playing={profile.autoplayHover && isHover}
+                url={video.videoUrl}
+                width={isHover ? '300px' : '200px'}
+                height={isHover ? '300px' : '200px'}
+                // active={currentProfile?.id === profile.id}
+            />
+        </div>
 
-//                 <div className='video-thumb-wrapper' onMouseEnter={()=> setIsHover(true)} onMouseLeave={() => setIsHover(false)} key={video.id}>
-//                     <ReactPlayer
-//                         volume={0}
-//                         playing={profile.autoplayHover && isHover}
-//                         url={video.videoUrl}
-//                         width='200px'
-//                         height='200px'
-//                         // active={currentProfile?.id === profile.id}
+        // map movies in carousels by genre
+    );
+}
 
-//                     />
-//                 </div>
-
-//         // map movies in carousels by genre
-//     );
-// }
-
-// export default VideoThumbnail;
+export default VideoThumbnail;
