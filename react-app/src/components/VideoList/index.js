a0326b23b2af131f08b6d6850e1a8ac1627f1fee
thisismydisplay <markrockwellosman@gmail.com>
2022-06-16 09:53:48 -0500

    added my list to videolist

--- react-app/src/components/VideoList/index.js
+++ react-app/src/components/VideoList/index.js
@@ -26,43 +26,62 @@ function VideoList({ videos, mylist }) {
             {mylist && (
                 <>
                     {myListVideos.map((video) => (
-                            <div
-                                className='video-thumb-container'
-                                key={video.id}
-                            >
-                                <VideoThumbnail
-                                    profile={profile}
-                                    video={video}
-                                />
-                            </div>
+                        <div className='video-thumb-container' key={video.id}>
+                            <VideoThumbnail profile={profile} video={video} />
+                        </div>
                     ))}
                 </>
             )}
-            {!mylist &&
-                genres.map((genre, i) => (
-                    <div className='video-carousel-wrapper' key={i}>
-                        <div className='video-list-title'>{genre}</div>
-                        <div className='video-list-wrapper'>
-                            {Object.values(videos).map((video) => (
-                                <div key={video.id}>
-                                    {video.genre === genre && (
-                                        <div
-                                            className='video-thumb-container'
-                                            // key={video.id}
-                                        >
-                                            <VideoThumbnail
-                                                profile={profile}
-                                                video={video}
-                                            />
-                                        </div>
-                                    )}
+
+            <div className='my-list-browse-container'>
+                {!mylist && myListVideos.length > 0 && (
+                    <div className='video-carousel-wrapper'>
+                        <div className='video-carousel-wrapper'>
+                            <div className='video-list-title'>My List</div>
+                            <div className='video-list-wrapper'>
+                            {myListVideos.map((video) => (
+                                <div
+                                    className='video-thumb-container'
+                                    key={video.id}
+                                >
+                                    <VideoThumbnail
+                                        profile={profile}
+                                        video={video}
+                                    />
                                 </div>
                             ))}
+                            </div>
                         </div>
                     </div>
-                ))}
+                )}
+            </div>
+
+            <div className='browse-genre-container'>
+                {!mylist &&
+                    genres.map((genre, i) => (
+                        <>
+                            <div className='video-list-title'>{genre}</div>
+                            <div className='video-list-wrapper'>
+                                {Object.values(videos).map((video) => (
+                                    <div key={video.id}>
+                                        {video.genre === genre && (
+                                            <div
+                                                className='video-thumb-container'
+                                                // key={video.id}
+                                            >
+                                                <VideoThumbnail
+                                                    profile={profile}
+                                                    video={video}
+                                                />
+                                            </div>
+                                        )}
+                                    </div>
+                                ))}
+                            </div>
+                        </>
+                    ))}
+            </div>
         </>
-        // map movies in carousels by genre
     );
 }
 
